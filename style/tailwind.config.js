02fd44011daa2914ff0f6368345884077238c15a
Azriel Hoh <azriel91@gmail.com>
2023-07-08 13:03:37 +1200

    Add tailwind css files and configuration.

---
+++ style/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Relative to workspace root.
+    'src/**/*.rs',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
