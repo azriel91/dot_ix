6fe996035f0944c5d14a371987d0edee25c1ce7d
Azriel Hoh <azriel91@gmail.com>
2023-07-15 17:11:33 +1200

    Use WASM compiled GraphViz to render graph instead of server function.

--- src/app/dot_svg.rs
+++ src/app/dot_svg.rs
@@ -1,64 +1,36 @@
-use leptos::*;
-
-#[server(DotSvgRender, "/api")]
-pub async fn dot_svg(dot_src: String) -> Result<(String, String), ServerFnError> {
-    use std::process::Stdio;
-    use tokio::io::{AsyncReadExt, AsyncWriteExt};
-
-    let mut dot_process = tokio::process::Command::new("dot")
-        .arg("-Tsvg")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
-        .expect("Failed to spawn dot command");
-
-    if let Some(mut stdin) = dot_process.stdin.take() {
-        stdin
-            .write_all(dot_src.as_bytes())
-            .await
-            .map_err(|error| ServerFnError::ServerError(format!("{error}")))?;
-    }
-
-    let mut dot_svg = String::with_capacity(dot_src.len());
-    if let Some(mut stdout) = dot_process.stdout.take() {
-        stdout
-            .read_to_string(&mut dot_svg)
-            .await
-            .map_err(|error| ServerFnError::ServerError(format!("{error}")))?;
-    }
-
-    let mut dot_stderr = String::new();
-    if let Some(mut stderr) = dot_process.stderr.take() {
-        stderr
-            .read_to_string(&mut dot_stderr)
-            .await
-            .map_err(|error| ServerFnError::ServerError(format!("{error}")))?;
-    }
-
-    dot_process
-        .wait()
-        .await
-        .map_err(|error| ServerFnError::ServerError(format!("{dot_stderr}{error}")))?;
-
-    Ok((dot_svg, dot_stderr))
-}
+use leptos::{html::Div, *};
+use leptos_meta::Script;
 
 /// Renders a graphviz graph as an SVG.
 #[component]
 pub fn DotSvg(cx: Scope, dot_src: ReadSignal<Option<String>>) -> impl IntoView {
-    let dot_svg_and_error_resource = create_resource(cx, dot_src, |dot_src| async move {
-        if let Some(dot_src) = dot_src {
+    // DOM elements for the graph and error
+    let svg_div_ref = create_node_ref::<Div>(cx);
+    let error_div_ref = create_node_ref::<Div>(cx);
+
+    create_effect(cx, move |_| {
+        if let Some(dot_src) = dot_src.get() {
             if !dot_src.is_empty() {
-                match dot_svg(dot_src).await {
-                    Ok((dot_svg, error_text)) => (dot_svg, error_text),
-                    Err(error) => (String::from(""), format!("{error}")),
+                if svg_div_ref.get().is_some() {
+                    #[cfg(target_arch = "wasm32")]
+                    if let Some(window) = web_sys::window() {
+                        let _ = window.set_timeout_with_callback(&js_sys::Function::new_no_args(
+                            &format!(
+                                r#"
+                                var dot_src = `{dot_src}`;
+                                try {{
+                                    var dot_svg = window.graphviz.layout(dot_src, "svg", "dot");
+                                    document.getElementById("svg_div").innerHTML = dot_svg;
+                                    document.getElementById("error_div").innerText = "";
+                                }} catch (error) {{
+                                    document.getElementById("error_div").innerText = error;
+                                }}
+                            "#
+                            ),
+                        ));
+                    }
                 }
-            } else {
-                (String::from(""), String::from(""))
             }
-        } else {
-            (String::from(""), String::from(""))
         }
     });
 
@@ -67,32 +39,19 @@ pub fn DotSvg(cx: Scope, dot_src: ReadSignal<Option<String>>) -> impl IntoView {
             fallback=move || view! { cx, <p>"Loading..."</p> }
         >
             <h2>"Graph"</h2>
-            {move || {
-                dot_svg_and_error_resource.read(cx)
-                    .map(|(dot_svg, error_text)| view! {
-                        cx,
-                        <div>
-                            <div inner_html=dot_svg />
+            <div>
+                <div id="svg_div" _ref=svg_div_ref />
+                <div id="error_div" _ref=error_div_ref />
 
-                            <div class={
-                                let error_text_empty = error_text.is_empty();
-                                move || {
-                                    if error_text_empty {
-                                        "hidden"
-                                    } else {
-                                        "
-                                        border
-                                        border-amber-300
-                                        bg-gradient-to-b from-amber-100 to-amber-200
-                                        rounded
-                                        "
-                                    }
-                                }
-                            }
-                            >{error_text}</div>
-                        </div>
-                    })
-            }}
+                // For explanation about ES6 modules and inline handlers,
+                // see <https://stackoverflow.com/a/59539045>
+                <Script type_="module">r#"
+                    import { Graphviz } from "https://cdn.jsdelivr.net/npm/@hpcc-js/wasm/dist/graphviz.js";
+                    const graphviz = await Graphviz.load();
+                    window.graphviz = graphviz;
+                "#
+                </Script>
+            </div>
         </Suspense>
     }
 }
