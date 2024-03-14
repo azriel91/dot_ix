f8bf1a3bd367fb94784ac50e9351ea668260fae0
Azriel Hoh <azriel91@gmail.com>
2024-03-14 18:58:36 +1300

    Gate `dot_ix_rt` and `dot_ix_web_components` behind features.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! Interactive dot graphs.
 
 pub use dot_ix_model as model;
+#[cfg(feature = "rt")]
 pub use dot_ix_rt as rt;
+#[cfg(feature = "web_components")]
 pub use dot_ix_web_components as web_components;
