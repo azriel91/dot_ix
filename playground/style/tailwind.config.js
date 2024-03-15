0749aa01ac68c91b7a30b8f97cd004e787cb7b41
Azriel Hoh <azriel91@gmail.com>
2024-03-16 12:24:22 +1300

    Adjust `tailwind.config.js` to tolerate different working directories.

--- playground/style/tailwind.config.js
+++ playground/style/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // Relative to workspace root.
-    'src/**/*.rs',
+    // Relative to workspace root / where you run `cargo leptos` from.
+    '**/src/**/*.rs',
   ],
   theme: {
     extend: {},
