e364fbe513b883096c6ca0618936dd8749b93aae
Azriel Hoh <azriel91@gmail.com>
2024-03-30 10:06:35 +1300

    Move `InfoGraphDot` into its own module.

--- crate/rt/src/lib.rs
+++ crate/rt/src/lib.rs
@@ -1,5 +1,6 @@
 //! Runtime logic for the `dot_ix` diagramming application.
 
-pub use self::into_graphviz_dot_src::IntoGraphvizDotSrc;
+pub use self::{info_graph_dot::InfoGraphDot, into_graphviz_dot_src::IntoGraphvizDotSrc};
 
+mod info_graph_dot;
 mod into_graphviz_dot_src;
