bfc6e856e90ce3000dbdb627103dc31eeac2acc2
Azriel Hoh <azriel91@gmail.com>
2024-06-28 14:43:50 +1200

    Add builder methods on `GraphvizAttrs`.

--- crate/model/src/common/graphviz_attrs.rs
+++ crate/model/src/common/graphviz_attrs.rs
@@ -46,32 +46,92 @@ impl GraphvizAttrs {
         Self::default()
     }
 
-    /// Returns the default constraint value for edges.
+    /// Sets the default [`constraint`] value for edges.
+    ///
+    /// [`constraint`]: https://graphviz.org/docs/attrs/constraint/
+    pub fn with_edge_constraint_default(mut self, edge_constraint_default: bool) -> Self {
+        self.edge_constraint_default = edge_constraint_default;
+        self
+    }
+
+    /// Sets the map of edge [`constraint`]s.
+    ///
+    /// [`constraint`]: https://graphviz.org/docs/attrs/constraint/
+    pub fn with_edge_constraints(mut self, edge_constraints: EdgeConstraints) -> Self {
+        self.edge_constraints = edge_constraints;
+        self
+    }
+
+    /// Sets the default [`dir`] value for edges.
+    ///
+    /// [`dir`]: https://graphviz.org/docs/attrs/dir/
+    pub fn with_edge_dir_default(mut self, edge_dir_default: EdgeDir) -> Self {
+        self.edge_dir_default = edge_dir_default;
+        self
+    }
+
+    /// Sets the map of edge [`dir`]s.
+    ///
+    /// [`dir`]: https://graphviz.org/docs/attrs/dir/
+    pub fn with_edge_dirs(mut self, edge_dirs: EdgeDirs) -> Self {
+        self.edge_dirs = edge_dirs;
+        self
+    }
+
+    /// Sets the default [`minlen`] value for edges.
+    ///
+    /// [`minlen`]: https://graphviz.org/docs/attrs/minlen/
+    pub fn with_edge_minlen_default(mut self, edge_minlen_default: u32) -> Self {
+        self.edge_minlen_default = edge_minlen_default;
+        self
+    }
+
+    /// Sets the map of edge [`minlen`]s.
+    ///
+    /// [`minlen`]: https://graphviz.org/docs/attrs/minlen/
+    pub fn with_edge_minlens(mut self, edge_minlens: EdgeMinlens) -> Self {
+        self.edge_minlens = edge_minlens;
+        self
+    }
+
+    /// Returns the default [`constraint`] value for edges.
+    ///
+    /// [`constraint`]: https://graphviz.org/docs/attrs/constraint/
     pub fn edge_constraint_default(&self) -> bool {
         self.edge_constraint_default
     }
 
-    /// Returns the map of edge constraints.
+    /// Returns the map of edge [`constraint`]s.
+    ///
+    /// [`constraint`]: https://graphviz.org/docs/attrs/constraint/
     pub fn edge_constraints(&self) -> &EdgeConstraints {
         &self.edge_constraints
     }
 
-    /// Returns the default dir value for edges.
+    /// Returns the default [`dir`] value for edges.
+    ///
+    /// [`dir`]: https://graphviz.org/docs/attrs/dir/
     pub fn edge_dir_default(&self) -> EdgeDir {
         self.edge_dir_default
     }
 
-    /// Returns the map of edge dirs.
+    /// Returns the map of edge [`dir`]s.
+    ///
+    /// [`dir`]: https://graphviz.org/docs/attrs/dir/
     pub fn edge_dirs(&self) -> &EdgeDirs {
         &self.edge_dirs
     }
 
-    /// Returns the default `minlen` value for edges.
+    /// Returns the default [`minlen`] value for edges.
+    ///
+    /// [`minlen`]: https://graphviz.org/docs/attrs/minlen/
     pub fn edge_minlen_default(&self) -> u32 {
         self.edge_minlen_default
     }
 
-    /// Returns the map of edge `minlen`s.
+    /// Returns the map of edge [`minlen`]s.
+    ///
+    /// [`minlen`]: https://graphviz.org/docs/attrs/minlen/
     pub fn edge_minlens(&self) -> &EdgeMinlens {
         &self.edge_minlens
     }
