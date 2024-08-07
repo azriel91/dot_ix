9a5232d7102c25a171481a62421decc6218712d7
Azriel Hoh <azriel91@gmail.com>
2024-08-07 15:24:47 +1200

    Don't reverse order of generated nodes, graphviz behaviour likely changed in `2.18.1`.

--- crate/rt/src/into_graphviz_dot_src/info_graph.rs
+++ crate/rt/src/into_graphviz_dot_src/info_graph.rs
@@ -142,9 +142,6 @@ impl IntoGraphvizDotSrc for &InfoGraph {
         let node_clusters = self
             .hierarchy()
             .iter()
-            // Reversing the order we feed nodes to Graphviz dot tends to produce a more natural
-            // layout order.
-            .rev()
             .map(|(node_id, node_hierarchy)| {
                 node_cluster(
                     self,
